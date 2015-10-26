f3ae1f062d22f228c9146aba82138c5a41d749a1
avwo <avwu@qq.com>
2015-10-26 13:58:51 +0800

    proxy

--- js/options.js
+++ js/options.js
@@ -1,22 +1,16 @@
-var bgWin = chrome.extension.getBackgroundPage();
-var selectedName = 'whistle';
-var WARN = '\n(不要勾选下面可能出现的`禁止此页再显示对话框`)';
+var win = chrome.extension.getBackgroundPage();
+var proxy = win.proxy;
+var activeName = localStorage.activeProxyName || 'whistle';
 
 var proxyList = $('#proxyList').on('click', 'a', function(e) {
 	var self = $(this);
-	if (self.hasClass('selected')) {
-		return false;
+	if (!self.hasClass('selected')) {
+		proxyList.find('a').removeClass('selected');
+		localStorage.activeProxyName = self.attr('data-name');
+		self.addClass('selected');
+		$('#proxyName').select().focus();
 	}
-	var name = self.attr('data-name');
-	if (name == 'whistle' || name == 'aeproxy') {
-		$('#removeProxy').hide();
-	} else {
-		$('#removeProxy').show();
-	}
-	//localStorage.selectedProxyName = self.attr('data-name');
-	proxyList.find('a').removeClass('selected');
-	self.addClass('selected');
-	$('#proxyName').select().focus();
+	
 	return false;
 }).on('mousedown', 'a', function() {
 	var self = $(this);
@@ -26,29 +20,24 @@ var proxyList = $('#proxyList').on('click', 'a', function(e) {
 });
 
 $('#createProxy').click(function() {
-	var name = $.trim(prompt('请输入不超过36个字符的代理名称：' + WARN));
-	if (!name) {
-		alert('名称不能为空' + WARN);
+	var name = $.trim(prompt('请输入不超过36个字符的代理名称：'));
+	if (!name || !(name = name.trim())) {
 		return false;
 	}
-	
-	if (name.length > 36) {
-		alert('名称不能超过36个字符' + WARN);
+	name = name.substring(0, 36);
+	if (proxy.getProxy(name)) {
+		alert('`' + name + '`已经存在，请重新创建');
 		return false;
 	}
-	
-	
-	
-	var item = createProxyItem(name).appendTo('#proxyList');
-	item.trigger('click');
+	createProxyElem(name).appendTo('#proxyList').trigger('click');
 	return false;
 });
 
-function createProxyItem(name) {
+function createProxyElem(name) {
 	var item = $('<a href="javascript:;">whistle</a>');
 	item.attr('data-name', name);
 	item.text(name);
 	return item;
 }
 
-proxyList.find('a[data-name="' + selectedName.replace(/(["\\])/g, '\\$1') +'"]').trigger('click');
+proxyList.find('a[data-name="' + activeName.replace(/(["\\])/g, '\\$1') +'"]').trigger('click');
