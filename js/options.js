7ecfd6791ed3c240a1ebfbe0b998221da43945fc
avwo <avwu@qq.com>
2015-10-26 14:06:41 +0800

    handle edit proxy

--- js/options.js
+++ js/options.js
@@ -1,12 +1,15 @@
 var win = chrome.extension.getBackgroundPage();
 var proxy = win.proxy;
-var activeName = localStorage.activeProxyName || 'whistle';
 
 var proxyList = $('#proxyList').on('click', 'a', function(e) {
 	var self = $(this);
 	if (!self.hasClass('selected')) {
 		proxyList.find('a').removeClass('selected');
-		localStorage.activeProxyName = self.attr('data-name');
+		var name = self.attr('data-name');
+		localStorage.activeProxyName = name;
+		$('#proxyName').val(name || '');
+		$('#proxyIP').val(self.attr('data-ip') || '');
+		$('#proxyPort').val(self.attr('data-port') || '');
 		self.addClass('selected');
 		$('#proxyName').select().focus();
 	}
@@ -19,6 +22,7 @@ var proxyList = $('#proxyList').on('click', 'a', function(e) {
 	}
 });
 
+var proxyList = $('#proxyList');
 $('#createProxy').click(function() {
 	var name = $.trim(prompt('请输入不超过36个字符的代理名称：'));
 	if (!name || !(name = name.trim())) {
@@ -29,15 +33,28 @@ $('#createProxy').click(function() {
 		alert('`' + name + '`已经存在，请重新创建');
 		return false;
 	}
-	createProxyElem(name).appendTo('#proxyList').trigger('click');
+	proxy.setProxy(name);
+	createProxyElem(name).appendTo(proxyList).trigger('click');
 	return false;
 });
 
-function createProxyElem(name) {
+function createProxyElem(name, host, port) {
 	var item = $('<a href="javascript:;">whistle</a>');
-	item.attr('data-name', name);
+	item.attr({
+		'data-name': name,
+		'data-host': host,
+		'data-port': port
+	});
+	
 	item.text(name);
 	return item;
 }
 
-proxyList.find('a[data-name="' + activeName.replace(/(["\\])/g, '\\$1') +'"]').trigger('click');
+function init() {
+	proxy.getProxyConfig.list.forEach(function(item) {
+		createProxyElem(item.name, item.host, item, port).appendTo(proxyList);
+	});
+	
+	var activeName = localStorage.activeProxyName || 'whistle';
+	proxyList.find('a[data-name="' + activeName.replace(/(["\\])/g, '\\$1') +'"]').trigger('click');
+}
