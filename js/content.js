ece3d4817e2806ec980f26da6f2db51b9f1cbbc0
avwo <avwu@qq.com>
2016-01-08 10:43:18 +0800

    avoid endless loop

--- js/content.js
+++ js/content.js
@@ -1,12 +1,14 @@
+var retryCount = 0;
 
 function getIp(index) {
 	chrome.runtime.sendMessage({type: 'getIp', index: index}, function(ip) {
 		if (typeof ip == 'string') {
 			showIp(ip);
-		} else {
+		} else if (retryCount < 3) {
 			setTimeout(function() {
+				retryCount++;
 				getIp(ip);
-			}, 500);
+			}, 500 + retryCount * 500);
 		}
 	});
 }
