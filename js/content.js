e02b554d3c2c87196f6f4d34c497d6be3da38c5f
avwo <avwu@qq.com>
2016-01-08 11:26:46 +0800

    show ip

--- js/content.js
+++ js/content.js
@@ -1,9 +1,13 @@
 var retryCount = 0;
+var id = '_whistle-' + Date.now() + '_';
 
 function getIp(index) {
 	chrome.runtime.sendMessage({type: 'getIp', index: index}, function(ip) {
 		if (typeof ip == 'string') {
 			showIp(ip);
+			setInterval(function() {
+				showIp(ip);
+			}, 2000);
 		} else if (ip > 0 && retryCount < 3) {
 			setTimeout(function() {
 				retryCount++;
@@ -14,7 +18,10 @@ function getIp(index) {
 }
 
 function showIp(ip) {
-	var elem = $('<span style="position: fixed; z-index: 999999999; right: 10px; bottom: 3px; line-height: 14px; padding-left: 16px; overflow: hidden; height: 14px; display: inline-block; font-size: 12px!important; color: #555; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">'
+	if ($('#' + id).length) {
+		return;
+	}
+	var elem = $('<span id="' + id + '" style="position: fixed; z-index: 999999999; right: 10px; bottom: 3px; line-height: 14px; padding-left: 16px; overflow: hidden; height: 14px; display: inline-block; font-size: 12px!important; color: #555; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">'
 			+ '<i title="close" style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-style: normal; position: absolute; left: 0; top: 0; font-size: 12px!important; line-height: 12px; overflow: hidden; height: 14px; margin-right: 3px; background: rgb(243,243,243); width: 14px; text-align: center; color: rgb(63,63,63); cursor: pointer; display: none; border-radius: 14px;">&times;</i>' + ip + '</span>')
 			.appendTo(document.body);
 	elem.on('mouseenter', function() {
