f690be47c42e7828a84fb3fc263733fd85172750
avwo <avwu@qq.com>
2016-01-08 20:28:54 +0800

    z-index: 2147483647

--- js/content.js
+++ js/content.js
@@ -21,7 +21,7 @@ function showIp(ip) {
 	if ($('#' + id).length) {
 		return;
 	}
-	var elem = $('<span id="' + id + '" style="position: fixed; z-index: 999999999; right: 10px; bottom: 3px; line-height: 14px; padding-left: 16px; overflow: hidden; height: 14px; display: inline-block; font-size: 12px!important; color: #555; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">'
+	var elem = $('<span id="' + id + '" style="position: fixed; z-index: 2147483647; right: 10px; bottom: 3px; line-height: 14px; padding-left: 16px; overflow: hidden; height: 14px; display: inline-block; font-size: 12px!important; color: #555; font-family: Helvetica Neue,Helvetica,Arial,sans-serif;">'
 			+ '<i title="close" style="font-family: Helvetica Neue,Helvetica,Arial,sans-serif; font-style: normal; position: absolute; left: 0; top: 0; font-size: 12px!important; line-height: 12px; overflow: hidden; height: 14px; margin-right: 3px; background: rgb(243,243,243); width: 14px; text-align: center; color: rgb(63,63,63); cursor: pointer; display: none; border-radius: 14px;">&times;</i><span style="display: inline-block; line-height: 14px; overflow: hidden; height: 14px;">' + ip + '</span></span>')
 			.appendTo(document.body);
 	elem.on('mouseenter', function() {
